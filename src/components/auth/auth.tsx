59c4ed7d660f97393c78dc75cfad76f4f959c2ba
VaibhavMatere <vpm00718897@gmail.com>
2024-07-13 01:01:20 +0530

    ✨feat signup & login page added

--- src/components/auth/auth.tsx
+++ src/components/auth/auth.tsx
@@ -1,33 +1,126 @@
+"use client";
+
 import React from "react";
 import { Button } from "../ui/button";
 
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
 
-const Auth = () => {
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  email: z.string().min(2).max(12),
+  password: z.string().min(6).max(20),
+});
+
+const AuthForm = ({ formType }: { formType: "Login" | "Signup" }) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    if (formType === "Login") {
+      console.log(values, "loggin in user");
+    } else {
+      console.log(values, "signing up user");
+    }
+  }
   return (
     <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="user@gmail.com" {...field} />
+                </FormControl>
+                <FormDescription>
+                  enter your registered email id
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input placeholder="xxxxx" {...field} />
+                </FormControl>
+                <FormDescription>enter your password here</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">
+            {" "}
+            {formType === "Signup" ? "Sign up" : "Login"}
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+};
+
+const Auth = () => {
+  return (
+    <div className="flex items-center space-x-3">
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="outline">Sign up</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>sigup to continue shopping</DialogTitle>
+          </DialogHeader>
+          <div>
+            <AuthForm formType={"Signup"} />
+          </div>
+        </DialogContent>
+      </Dialog>
       <Dialog>
         <DialogTrigger asChild>
           <Button variant="outline">Login</Button>
         </DialogTrigger>
         <DialogContent>
           <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
-            <DialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </DialogDescription>
+            <DialogTitle>Login to continue shopping</DialogTitle>
           </DialogHeader>
+          <div>
+            <AuthForm formType={"Login"} />
+          </div>
         </DialogContent>
       </Dialog>
-    </>
+    </div>
   );
 };
 
