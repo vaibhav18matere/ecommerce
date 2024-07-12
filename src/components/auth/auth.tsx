425ca12689868a75ab4db06673e733ab9d97d84e
VaibhavMatere <vpm00718897@gmail.com>
2024-07-13 00:22:36 +0530

    login btn & dialogue box added

---
+++ src/components/auth/auth.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Button } from "../ui/button";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+const Auth = () => {
+  return (
+    <>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="outline">Login</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
+
+export default Auth;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { MOCKDATA } from "@/lib/mockdata";
+import Auth from "@/components/auth/auth";
 
 export default function Home() {
   return (
@@ -12,24 +12,27 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="mx-auto flex w-full max-w-screen-xl flex-col items-center justify-center">
-        <header className="flex items-center justify-between w-full bg-slate-200 p-10">
+        <header className="flex w-full items-center justify-between bg-slate-200 p-10">
           <div>
             <Link href={"/admin"}>Admin Dashboard</Link>
           </div>
           <div className="flex items-center space-x-2">
-            <Button variant="outline">Signup</Button>
-            <Button variant="outline">Login</Button>
+            <Auth />
           </div>
         </header>
       </div>
-      <div className="flex flex-row flex-wrap gap-8 justify-center my-8">
+      <div className="my-8 flex flex-row flex-wrap justify-center gap-8">
         {MOCKDATA.map((product) => (
           <div
             key={product.id}
             className="flex max-w-96 flex-col items-start gap-4 rounded-xl border border-gray-300 bg-white p-4 shadow-lg"
           >
             <div>
-              <img className="overflow-hidden w-96 h-full" src={product.imageURL} alt={product.name} />
+              <img
+                className="h-full w-96 overflow-hidden"
+                src={product.imageURL}
+                alt={product.name}
+              />
             </div>
             <div>{product.name}</div>
             <div>{product.description}</div>
