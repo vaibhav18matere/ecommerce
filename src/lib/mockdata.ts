dd1c1cc8776212efc4008d8a03db81cb8302f7c5
VaibhavMatere <vpm00718897@gmail.com>
2024-07-13 00:10:30 +0530

    mockdata added

---
+++ src/lib/mockdata.ts
@@ -0,0 +1,81 @@
+export const MOCKDATA = [
+  {
+    id: 1,
+    name: "Longos - Chicken Wings",
+    description:
+      "Fusce consequat. Nulla nisl. Nunc nisl. Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum. In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante.",
+    imageURL: "http://dummyimage.com/205x100.png/5fa2dd/ffffff",
+    quantityAvailable: 75,
+  },
+  {
+    id: 2,
+    name: "Chicken - Base",
+    description:
+      "In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet. Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui. Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc.",
+    imageURL: "http://dummyimage.com/176x100.png/5fa2dd/ffffff",
+    quantityAvailable: 55,
+  },
+  {
+    id: 3,
+    name: "Tomatoes - Cherry, Yellow",
+    description:
+      "Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus. Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.",
+    imageURL: "http://dummyimage.com/169x100.png/ff4444/ffffff",
+    quantityAvailable: 70,
+  },
+  {
+    id: 4,
+    name: "Cheese - Gouda",
+    description:
+      "Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum. Curabitur in libero ut massa volutpat convallis.",
+    imageURL: "http://dummyimage.com/199x100.png/5fa2dd/ffffff",
+    quantityAvailable: 32,
+  },
+  {
+    id: 5,
+    name: "Pepper - Black, Whole",
+    description: "Fusce consequat.",
+    imageURL: "http://dummyimage.com/179x100.png/ff4444/ffffff",
+    quantityAvailable: 57,
+  },
+  {
+    id: 6,
+    name: "Muffin - Mix - Bran And Maple 15l",
+    description:
+      "Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.",
+    imageURL: "http://dummyimage.com/215x100.png/ff4444/ffffff",
+    quantityAvailable: 74,
+  },
+  {
+    id: 7,
+    name: "Sage - Rubbed",
+    description:
+      "Suspendisse accumsan tortor quis turpis. Sed ante. Vivamus tortor. Duis mattis egestas metus. Aenean fermentum.",
+    imageURL: "http://dummyimage.com/232x100.png/5fa2dd/ffffff",
+    quantityAvailable: 34,
+  },
+  {
+    id: 8,
+    name: "Pepper - Pablano",
+    description:
+      "Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus. Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis.",
+    imageURL: "http://dummyimage.com/231x100.png/cc0000/ffffff",
+    quantityAvailable: 17,
+  },
+  {
+    id: 9,
+    name: "Curry Powder",
+    description:
+      "Mauris sit amet eros. Suspendisse accumsan tortor quis turpis. Sed ante. Vivamus tortor. Duis mattis egestas metus. Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.",
+    imageURL: "http://dummyimage.com/212x100.png/cc0000/ffffff",
+    quantityAvailable: 17,
+  },
+  {
+    id: 10,
+    name: "Liners - Banana, Paper",
+    description:
+      "Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.",
+    imageURL: "http://dummyimage.com/166x100.png/dddddd/000000",
+    quantityAvailable: 96,
+  },
+];
