de9a10644094403fb166cebffbb2e3ba2276068c
VaibhavMatere <vpm00718897@gmail.com>
2024-07-20 00:25:03 +0530

    admin dashboard page added

---
+++ src/pages/admin/dashboard.tsx
@@ -0,0 +1,39 @@
+import AdminLayout from "@/components/layouts/admin-layout";
+import React from "react";
+import { MOCKDATA } from "@/lib/mockdata";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+const AdminDashboard = () => {
+  return (
+    <>
+      <AdminLayout>
+        <div className="mt-28 p-10">
+          <div className="my-6 flex w-full flex-row justify-end">
+            <Button size={"lg"}>Add New Product</Button>
+          </div>
+          {MOCKDATA.map((product) => (
+            <Link
+              key={product.id}
+              href={`/${product.slug}`}
+              className="flex max-w-96 flex-col items-start gap-4 rounded-xl border border-gray-300 bg-white p-4 shadow-lg hover:cursor-pointer hover:border-black"
+            >
+              <div>
+                <img
+                  className="h-full w-96 overflow-hidden"
+                  src={product.imageURL}
+                  alt={product.name}
+                />
+              </div>
+              <div>{product.name}</div>
+              <div>{product.description}</div>
+              <div>Available Quantitys : {product.quantityAvailable}</div>
+            </Link>
+          ))}
+        </div>
+      </AdminLayout>
+    </>
+  );
+};
+
+export default AdminDashboard;
