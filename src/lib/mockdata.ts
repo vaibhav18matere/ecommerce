3100be70d6385be5f40afe025f9bd1076fd1bad7
VaibhavMatere <vpm00718897@gmail.com>
2024-07-19 19:43:02 +0530

    slugs data added

--- src/lib/mockdata.ts
+++ src/lib/mockdata.ts
@@ -6,6 +6,7 @@ export const MOCKDATA = [
       "Fusce consequat. Nulla nisl. Nunc nisl. Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum. In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante.",
     imageURL: "http://dummyimage.com/205x100.png/5fa2dd/ffffff",
     quantityAvailable: 75,
+    slug: "product-1",
   },
   {
     id: 2,
@@ -14,6 +15,7 @@ export const MOCKDATA = [
       "In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet. Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui. Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc.",
     imageURL: "http://dummyimage.com/176x100.png/5fa2dd/ffffff",
     quantityAvailable: 55,
+    slug: "product-2",
   },
   {
     id: 3,
@@ -22,6 +24,7 @@ export const MOCKDATA = [
       "Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus. Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.",
     imageURL: "http://dummyimage.com/169x100.png/ff4444/ffffff",
     quantityAvailable: 70,
+    slug: "product-3",
   },
   {
     id: 4,
@@ -30,6 +33,7 @@ export const MOCKDATA = [
       "Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum. Curabitur in libero ut massa volutpat convallis.",
     imageURL: "http://dummyimage.com/199x100.png/5fa2dd/ffffff",
     quantityAvailable: 32,
+    slug: "product-4",
   },
   {
     id: 5,
@@ -37,6 +41,7 @@ export const MOCKDATA = [
     description: "Fusce consequat.",
     imageURL: "http://dummyimage.com/179x100.png/ff4444/ffffff",
     quantityAvailable: 57,
+    slug: "product-5",
   },
   {
     id: 6,
@@ -45,6 +50,7 @@ export const MOCKDATA = [
       "Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.",
     imageURL: "http://dummyimage.com/215x100.png/ff4444/ffffff",
     quantityAvailable: 74,
+    slug: "product-6",
   },
   {
     id: 7,
@@ -53,6 +59,7 @@ export const MOCKDATA = [
       "Suspendisse accumsan tortor quis turpis. Sed ante. Vivamus tortor. Duis mattis egestas metus. Aenean fermentum.",
     imageURL: "http://dummyimage.com/232x100.png/5fa2dd/ffffff",
     quantityAvailable: 34,
+    slug: "product-7",
   },
   {
     id: 8,
@@ -61,6 +68,7 @@ export const MOCKDATA = [
       "Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus. Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis.",
     imageURL: "http://dummyimage.com/231x100.png/cc0000/ffffff",
     quantityAvailable: 17,
+    slug: "product-8",
   },
   {
     id: 9,
@@ -69,6 +77,7 @@ export const MOCKDATA = [
       "Mauris sit amet eros. Suspendisse accumsan tortor quis turpis. Sed ante. Vivamus tortor. Duis mattis egestas metus. Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.",
     imageURL: "http://dummyimage.com/212x100.png/cc0000/ffffff",
     quantityAvailable: 17,
+    slug: "product-9",
   },
   {
     id: 10,
@@ -77,5 +86,6 @@ export const MOCKDATA = [
       "Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.",
     imageURL: "http://dummyimage.com/166x100.png/dddddd/000000",
     quantityAvailable: 96,
+    slug: "product-10",
   },
 ];
